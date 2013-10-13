ec105c0e3225a3412546c54290073a87ef396938
Alan <speedofsloth@gmail.com>
2013-10-13 17:09:03 +0100

    first commit

---
+++ walkinfo.php
@@ -0,0 +1,63 @@
+<?
+
+function db_connect() {
+$config['db'] = array(
+	'host'		=> 'localhost',
+	'username'	=> 'administrator',
+	'password'	=> 'parad1Gm',
+	'dbname'	=> 'nordic'
+	);
+$nordic_db= new PDO('mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['dbname'], $config['db']['username'],$config['db']['password']);
+return $nordic_db;
+}
+function walkFetch() {
+	$nordic_db=db_connect();
+	$query = $nordic_db->prepare("select `location` , `meet` , `directions` , `details` , `dogs` , `wtime` , `distance` , `imageurl` , `url` from `walks`");
+	$query->execute();
+	while($data= $query ->fetch(PDO::FETCH_ASSOC)) {
+
+			
+
+          echo "<div class='routes'>";
+          	echo "<table border=1>";
+          		echo "<tr>";
+          			echo "<td>Location:</td>";
+
+          			echo "<td>$data[location]</td>";
+          		echo "</tr>";
+          		echo "<tr>";
+          			echo "<td>Meeting Point:</td>";
+          			echo "<td>$data[meet]</td>";
+          		echo "</tr>";
+          		echo "<tr>";
+          			echo "<td>Directions:</td>";
+          			echo "<td>$data[directions]</td>";
+          		echo "</tr>";
+          		echo "<tr>";
+          			echo "<td>About:</td>";
+          			echo "<td>$data[details]</td>";
+          		echo "</tr>";
+          		echo "<tr>";
+          			echo "<td>Dogs:</td>";
+          			echo "<td>$dogs</td>";
+          		echo "</tr>";
+          		echo "<tr>";
+          			echo "<td>Walk Time:</td>";
+          			echo "<td>$data[wtime]</td>";
+          		echo "</tr>";
+          		echo "<tr>";
+          			echo "<td>Distance:</td>";
+          			echo "<td>$data[distance]</td>";
+          		echo "</tr>";
+          		echo "<tr>";
+          			echo "<td>Map:</td>";
+          			echo "<td>$data[url]</td>";
+          		echo "</tr>";
+          	echo "</table>";
+          echo "</div>";
+    }
+	
+	
+} 	
+walkFetch();
+?>
